cefdb829c090a89ffa176b739cad567562e58f98
Sanaa11111 <115801270+Sanaa11111@users.noreply.github.com>
2022-10-21 16:03:40 +0200

    modif printf
    modification au niveau du code

--- features/step_definititons/picard.rb
+++ features/step_definititons/picard.rb
@@ -3,7 +3,7 @@ Given(/^I am a prospect, I would like to access the Picard website$/) do
   require 'net/http'
 end
 When(/^I run the site in guest mode$/) do
-  printf "I run the site in guest mode"
+  printf "I run the site in guest mode---"
 end
 
 Then(/^the home page is displayed$/) do
